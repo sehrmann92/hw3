672bc9f76ba6dfc4297d8266e4e994c8b43d6a2a
Silvia Ehrmann <Silvia Ehrmann>
2015-11-12 21:32:03 +0100

    program with pointer and references

--- statistics.cxx
+++ statistics.cxx
@@ -3,19 +3,39 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
 
-int main(){
+void randnr(double* p, int N);
+void calc(double& mean, double& var,double* p, int N);
+
+int main()
+{
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
-
+   randnr(p,N);
+   calc(mean,var,p,N);
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
    return 0;
+}
+
+void randnr(double* p, int N)
+{
+	for(int i=0;i<N;i++)
+		p[i]=double(rand())/RAND_MAX;
+}
+
+void calc(double& mean, double& var,double* p, int N)
+{
+	mean=0.0;
+	for(int i=0;i<N;i++)
+		mean+=p[i];
+	mean/=N;
+	
+	var=0.0;
+	for(int i=0;i<N;i++)
+		var+=(p[i]-mean)*(p[i]-mean);
+	var/=N;
 }
